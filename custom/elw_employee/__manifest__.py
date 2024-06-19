c3bfa1a0f5bcae6a5a725c78159ce0926a96ab06
junwei-dev <junwei.er@digitalbigbite.com>
2024-06-19 14:43:43 +0800

    Adjusted employee fields before implementing features

--- custom/elw_employee/__manifest__.py
+++ custom/elw_employee/__manifest__.py
@@ -15,11 +15,13 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'ELW/ELW Human Resource',
+    'category': 'ELW/ELW Employee',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'stock', 'purchase', 'sale'],
+    'depends': [
+        #'base', 'product', 'stock', 'purchase', 'sale'
+        ],
 
     # always loaded
     'data': [
--- custom/elw_employee/models/employee.py
+++ custom/elw_employee/models/employee.py
@@ -5,6 +5,8 @@ class Employee(models.Model):
     _description = 'Custom Employee Model'
 
     name = fields.Char(string='Name', required=True)
+    email = fields.Char(string='Email')
+    phone = fields.Char(string='Phone')
     job_title = fields.Char(string='Job Title')
     department = fields.Many2one('hr.department', string='Department')
-    email = fields.Char(string='Email')
+
