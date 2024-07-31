673be40a3399dfcf0272cc5c7ae63fdb49cd507f
junwei-dev <junwei.er@digitalbigbite.com>
2024-07-31 13:40:11 +0800

    ERP Module Installation adjustments
    Installing ERP Module will hide all modules that needs premium to run and activate the remaining available ones

--- custom/elw_mrp/models/mrp_production.py
+++ custom/elw_mrp/models/mrp_production.py
@@ -8,14 +8,9 @@ class MrpProduction(models.Model):
     analysis_name = fields.Char(string='Analysis Name')
     quantity_produced = fields.Float(string='Quantity Produced', compute='_compute_quantity_produced', store=True)
     production_date = fields.Date(string='Production Date', default=fields.Date.context_today)
-    is_kits = fields.Boolean(string='Is Kit', compute='_compute_is_kits', store=True)
     product_name = fields.Char(string='Product Name', compute='_compute_product_name', store=True)
-
-    @api.depends('product_id')
-    def _compute_is_kits(self):
-        for record in self:
-            bom = self.env['mrp.bom']._bom_find(record.product_id, bom_type='phantom')
-            record.is_kits = bool(bom)
+    total_cost = fields.Float(string='Total Cost', compute='_compute_costs')
+    cost_per_unit = fields.Float(string='Cost / Unit', compute='_compute_cost_per_unit', store=True)
 
     @api.depends('product_id', 'production_date')
     def _compute_quantity_produced(self):
@@ -42,3 +37,18 @@ class MrpProduction(models.Model):
     def _compute_product_name(self):
         for record in self:
             record.product_name = record.product_id.name if record.product_id else ''
+
+    @api.depends('move_raw_ids', 'product_qty')
+    def _compute_costs(self):
+        for production in self:
+            # Calculate total cost
+            total_cost = sum(move.price_unit * move.product_uom_qty for move in production.move_raw_ids)
+            production.total_cost = total_cost
+
+    @api.depends('product_qty', 'total_cost')
+    def _compute_cost_per_unit(self):
+        for record in self:
+            if record.product_qty > 0:
+                record.cost_per_unit = record.total_cost / record.product_qty
+            else:
+                record.cost_per_unit = 0.0
