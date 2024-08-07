870bf01afb3d797e0fbf3f8f2c9c2a7b5ec63cc4
junwei-dev <junwei.er@digitalbigbite.com>
2024-08-07 17:05:37 +0800

    Update journal actions

--- custom/elw_accounting/__manifest__.py
+++ custom/elw_accounting/__manifest__.py
@@ -3,9 +3,10 @@
     'version': '1.0',
     'summary': 'Custom Accounting Module',
     'description': 'A module to manage custom accounting features.',
-    'category': 'Accounting',
+    'category': 'ELW/ELW Accounting',
     'author': 'Your Name',
     'website': 'http://www.yourwebsite.com',
+    'sequence': -100,
     'depends': ['base', 'account'],
     'data': [
         'views/accounting.xml',
--- custom/elw_accounting/models/accounting.py
+++ custom/elw_accounting/models/accounting.py
@@ -4,10 +4,3 @@ from odoo import models, fields, api
 class Accounting(models.Model):
     _inherit = 'account.account'
 
-    custom_field = fields.Char(string='Custom Field')
-
-    @api.onchange('custom_field')
-    def _onchange_custom_field(self):
-        # Custom logic for the field
-        if self.custom_field:
-            self.name = f'Custom: {self.custom_field}'
