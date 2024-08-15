f49581dd925d1cd0c02791fe016b0295a2284401
junwei-dev <junwei.er@digitalbigbite.com>
2024-08-15 14:00:51 +0800

    Unit of measurement

--- custom/elw_mrp/models/mrp_bom.py
+++ custom/elw_mrp/models/mrp_bom.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
-    uom_id = fields.Many2one('uom.uom', string="Unit of Measure")
+    uom_id = fields.Many2one('uom.uom', string="Unit of Measurement")
